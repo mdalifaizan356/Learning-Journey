322bbc37d40a698b96060c9368c4f72dea4fd4ff
faizanpasha356 <mdalifaizan356@gmail.com>
2024-08-09 01:29:19 +0530

    Re-Upload All Folders & Files

---
+++ JavaScript Self Learning/30 Days Challenge/Day 1.js
@@ -0,0 +1,78 @@
+/*
+Day 1: Variables and Data Types
+
+Tasks/Activities:
+    Activity 1: Variable Declaration
+        Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+        Task 2: Declare a variable using let, assign it a string, and log the value to the console.
+
+    Activity 2: Constant Declaration
+        Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+    
+    Activity 3: Data Types
+        Task 4: Create variables of different data types (number, string, boolean, object, array) and log each variable's type 
+                using the typeof operator.
+
+    Activity 4: Reassigning Variables
+        Task 5: Declare a variable using let, assign it an initial value, reassign a new value, and log both values to the 
+                console.
+
+    Activity 5: Understanding const
+        Task 6: Try reassigning a variable declared with const and observe the error.
+
+
+Feature Request:
+    1. Variable Types Console Log: Write a script that declares variables of different data types and logs both the value and type of each variable to the console.
+    2. Reassignment Demo: Create a script that demonstrates the difference in behavior between let and const when it comes to reassignment.
+
+
+Achievement:
+
+    By the end of these activities, you will:
+    Know how to declare variables using var, let, and const.
+    Understand the different data types in JavaScript.
+    Be able to use the typeof operator to identify the data type of a variable.
+    Understand the concept of variable reassignment and the immutability of const variables.
+
+*/
+
+//Task 1 =>
+    var number = 5;
+    console.log(number);
+
+//Task 2 =>
+    let Str = "Faizan";
+    console.log(Str);
+
+//Task 3 =>
+    const truthyVal = true;
+    console.log(truthyVal);
+
+//Task 4 =>
+    var myNumber = 10;
+    var myStr = "Ali";
+    var myBool = false;
+    var myObj = {Name:'Faizan', Batch:'JavaScript'};
+    var myArray = [1,2,3,4,5];
+    var myUndifineVariable;
+    var myNullValue= null;
+
+    console.log(typeof(myNumber));
+    console.log(typeof(myStr));
+    console.log(typeof(myBool));
+    console.log(typeof(myObj));
+    console.log(typeof(myArray));
+    console.log(typeof(myUndifineVariable));
+    console.log(typeof(myNullValue));
+
+//Task 5 =>
+    let myVar = "Faizan";
+    console.log(myVar);
+    myVar = "Ali";
+    console.log(myVar);
+
+//Task 6 =>
+    const myVar2 = "Faizan";
+    console.log(myVar);
+    myVar2 = "Ali";
+    console.log(myVar);
---
+++ JavaScript Self Learning/Js with chaiORcode/File1.js
@@ -0,0 +1,87 @@
+/*
+Javascript Documentation:
+                    MDN Js Documentation: It is easy but not original document bcoz it is from mozilla.org.
+                    TC39.ES: It is origional documentation. 
+
+Javascript Runtime Environment: Node.js and Deno founder of Ryan Dahl.
+
+let and var use to define scope of variable
+Prefer not to use var becouse of issue in block scope and functional scope
+Jab varible ko declare kar k usme kisi value ko assign nahi karte he to return me undefine milta he. 
+
+"use strict";    //Treat all js code as newer version 
+
+Data types in Javascript:
+    Primitive 
+                    number
+                    bigInt
+                    string
+                    boolean
+                    null
+                    undifined
+                    symbol
+    Non Primitive
+                    Array
+                    Objects
+
+Sabhi primitive data type call by value hote he.Matlb jab bhi hum value ko use karte he ya kahi se kahi pe copy
+karte he to memory se original data ka reference nahi diya he data ki copy di jati he.
+
+undefined means a variable has been declared but has not yet been assigned a value. typeof undefined is undifined
+null is representation of empty value. type of null is object.
+
+
+
+
+Question:
+What is Javascript Runtime Environment?
+Difference between let var and const?
+How to use alert in node.js?
+Difference between null and undifined?
+What is NaN?
+*/
+
+
+
+// const accountId = 144553;
+// let accountEmail = "faizan@gmail.com";
+// var accountPassword = "12345";
+// accountCity = "Jaipur";
+
+// let accountState;
+
+// console.log(accountId);
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+// console.log(typeof accountEmail);
+
+// let score =  true  //null   //"30"   //"30abc";
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber), valueInNumber);   //Amazing
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(+true);
+// console.log(-true);
+// console.log(+false);
+// console.log(-false);
+// console.log(+"");
+// console.log(-"");
+// num1 = num2 = num3 =2+2;
+
+// let counter = 100;
+// counter++;
+// console.log(counter);
+// ++counter
+// console.log(counter);
+
+// console.log(null >= 0);      //Yaha null ki value 0 ho jayegi
+// console.log(undefined > 0);
