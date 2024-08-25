bee11e5a6d3bb300c47161c85f2abd33f60de2dd
faizanpasha356 <mdalifaizan356@gmail.com>
2024-08-25 23:38:38 +0530

    Change File and Folder Structure

---
+++ Javascript/file1.js
@@ -0,0 +1,28 @@
+ /*
+ =========================================Notes=======================================================
+Javascript Documentation:
+    MDN Js Documentation: It is easy but not original document bcoz it is from mozilla.org.
+    TC39.ES: It is origional documentation. 
+
+Javascript History:
+
+Javascript Convention:
+    JavaScript me semicolon optional hota he. lekin hame use karna chahiye achchi practice mani jati he.
+    Use Variable name in camelCase.
+    Use class name in PascalCase.
+
+ 
+*/
+
+// "use strict";
+
+// console.log("Use Double quote with semicolon;");
+// console.log('Use single quote with semicolon ;');
+// console.log(`Use backtick with semicolon ;`);
+
+// var myName = "Uzzi";
+// console.log("Use Double quote without semicolon ;")
+// console.log('Use single quote without semicolon ;')
+// console.log(`Use backtick without semicolon ${myName} x;`)
+
+
