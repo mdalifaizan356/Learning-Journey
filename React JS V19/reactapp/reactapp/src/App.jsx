16a59d8cc69c902173fc3f108ad46f5a782496cb
faizanpasha356 <mdalifaizan356@gmail.com>
2024-09-11 23:21:52 +0530

    Learn React

--- React JS V19/reactapp/reactapp/src/App.jsx
+++ React JS V19/reactapp/reactapp/src/App.jsx
@@ -1,35 +1,28 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+// import banner = '../public/banner1.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src="../public/images/banner1.jpg" alt="bannerimg" width="50%" height="40%"/>
+        <MyFunction />
+        <MyFunction />
+        <MyFunction />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
+
+const MyFunction =()=>{
+  return(
+    <>
+      <h1>"Hii Faizan"</h1>
+    </>
+  )
+}
 export default App
